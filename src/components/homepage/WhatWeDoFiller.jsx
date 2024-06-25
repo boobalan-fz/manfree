a4037b8c8fb6eb1b5fa4d1b00ffad28c3fe6a5d3
boobalan-fz <boobalan.ramasamy@frenzo.io>
2024-06-25 18:54:41 +0530

    Home page fix

--- src/components/homepage/WhatWeDoFiller.jsx
+++ src/components/homepage/WhatWeDoFiller.jsx
@@ -1,15 +1,22 @@
 import Image from "next/image";
 
-const WhatWeDoFiller = ({ img, title, children }) => {
+const WhatWeDoFiller = ({ img, title, children, right = false }) => {
   return (
-    <div className="grid grid-cols-2 gap-10 px-20">
-      <div>
-        <Image src={img} alt="one" />
-      </div>
-      <div>
-        <h1 className="text-4xl font-semibold">{title}</h1>
+    <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10 px-14">
+      {!right && (
+        <div className="order-1 lg:order-none">
+          <Image src={img} alt="one" className="h-full" />
+        </div>
+      )}
+      <div className="order-2 lg:order-none">
+        <h1 className="text-4xl font-semibold tracking-tight">{title}</h1>
         <div className="flex flex-col items-start gap-4">{children}</div>
       </div>
+      {right && (
+        <div className="order-1 lg:order-none">
+          <Image src={img} alt="one" className="h-full" />
+        </div>
+      )}
     </div>
   );
 };
