602c6f69455f679b8c9c23990a946fee175ee08a
boobalan-fz <boobalan.ramasamy@frenzo.io>
2024-06-26 11:20:41 +0530

    small change

--- src/components/homepage/StatsCard.jsx
+++ src/components/homepage/StatsCard.jsx
@@ -6,7 +6,7 @@ const StatsCard = ({ count, title }) => {
       style={{
         backgroundImage: `url(${cardBg.src})`,
       }}
-      className="bg-cover bg-center p-10 bg-white flex justify-center items-center gap-5 flex-col rounded-md w-[340px] "
+      className="bg-cover bg-center p-10 bg-white flex justify-center items-center gap-5 flex-col rounded-md w-[329px] "
     >
       <h1 className="font-black text-5xl">{count}+</h1>
       <h1 className="text-2xl font-semibold">{title}</h1>
