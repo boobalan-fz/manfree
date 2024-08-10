2c99cacb72affbbd472c1d841052c844e2fa4279
boobalan-fz <boobalan.ramasamy@frenzo.io>
2024-08-10 20:03:02 +0530

    changes in package

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect(process.env.NEXT_MONGODB_URI);
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
