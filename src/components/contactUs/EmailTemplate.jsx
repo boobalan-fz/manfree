99249e785f319910a0a9c02b03d0712c16809912
boobalan-fz <boobalan.ramasamy@frenzo.io>
2024-09-17 20:19:22 +0530

    Finished

--- src/components/contactUs/EmailTemplate.jsx
+++ src/components/contactUs/EmailTemplate.jsx
@@ -84,11 +84,11 @@ export const YelpRecentLoginEmail = ({
                 </Text>
 
                 <Text style={paragraph}>
-                  If this was you, there's nothing else you need to do.
+                  If this was you, there&apos;s nothing else you need to do.
                 </Text>
                 <Text style={{ ...paragraph, marginTop: -5 }}>
-                  If this wasn't you or if you have additional questions, please
-                  see our support page.
+                  If this wasn&apos;t you or if you have additional questions,
+                  please see our support page.
                 </Text>
               </Column>
             </Row>
